2c33c1e82b1a366d9de2055a55dfa9b71143d5cc
thuannh1027 <thuannh.1027@gmail.com>
2018-07-17 02:10:40 +0700

    MinhTranFU - load coupons from api for list coupons screen

--- app/screens/coupon/listCoupon.js
+++ app/screens/coupon/listCoupon.js
@@ -1,97 +1,112 @@
 import React from 'react';
 import {
-    FlatList,
-    Image,
-    View,
-    TouchableOpacity
+  FlatList,
+  Image,
+  View,
+  TouchableOpacity
 } from 'react-native';
 import {
-    RkText,
-    RkCard,
-    RkStyleSheet
+  RkText,
+  RkCard,
+  RkStyleSheet,
+  RkTheme
 } from 'react-native-ui-kitten';
 import { FontAwesome } from '../../assets/icons';
-import { SocialBar } from '../../components';
 import { data } from '../../data';
-let moment = require('moment');
+import couponApi from '../../api/couponApi';
+import { UIConstants } from '../../config/appConstants'
 
 export class ListCoupon extends React.Component {
-    static navigationOptions = {
-        title: 'List Coupon'.toUpperCase()
-    };
+  static navigationOptions = {
+    title: 'List Coupon'.toUpperCase()
+  };
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            isLiked: false,
-        }
-        this.data = data.getArticles();
-        this.renderItem = this._renderItem.bind(this);
+    this.state = {
+      isLiked: false,
+      data: []
     }
+    // this.data = data.getArticles();
+    this.renderItem = this._renderItem.bind(this);
+  }
 
-    _keyExtractor(post, index) {
-        return post.id;
-    }
+  _keyExtractor(coupon, index) {
+    return coupon.id;
+  }
 
-    like() {
-        this.setState({
-            isLiked: !this.state.isLiked,
-        })
-    }
+  like() {
+    this.setState({
+      isLiked: !this.state.isLiked,
+    })
+  }
 
-    _renderItem(info) {
-        return (
-            <TouchableOpacity
-                delayPressIn={70}
-                activeOpacity={0.8}
-                onPress={() => this.props.navigation.navigate('CouponView')}>
-                <RkCard rkType='imgBlock' style={styles.card}>
-                    <Image rkCardImg source={{ uri: 'https://cf.shopee.vn/file/eeca8f01a851a50c99ff019a9ebdaa95' }} />
+  async componentWillMount() {
+    const res = await couponApi.getPage(1, 10)
+    const data = await res.json()
+    this.setState({
+      data
+    })
+  }
 
-                    <View rkCardImgOverlay rkCardContent style={styles.overlay}>
-                        <RkText rkType='header4 inverseColor'>CGV Coupon</RkText>
-                        <RkText style={styles.time}
-                            rkType='secondary2 inverseColor'>200000 VND</RkText>
-                        <Image source={{ uri: '../../img/photo32.jpg' }} />
-                    </View>
-                    <View style={{paddingVertical: 15,}}>
-                        <View style={{ alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row', marginHorizontal: 50, }}>
-                            <RkText rkType='awesome primary' onPress={() => this.like()}>
-                                {this.state.isLiked ? FontAwesome.heart : FontAwesome.emptyHeart}
-                            </RkText>
-                            <RkText style={{ marginLeft: 30, }} rkType='awesome primary' onPress={() => this.props.navigation.navigate('Comments')}>
-                                {FontAwesome.comment}
-                            </RkText>
-                        </View>
-                    </View >
-                </RkCard>
-            </TouchableOpacity>
-        )
-    }
+  _renderItem(data) {
+    const coupon = data.item
+    const image = coupon.images[0]
+    console.log(coupon)
 
-    render() {
-        return (
-            <FlatList
-                data={this.data}
-                renderItem={this.renderItem}
-                keyExtractor={this._keyExtractor}
-                style={styles.container} />
+    return (
+      <TouchableOpacity
+        delayPressIn={70}
+        activeOpacity={0.8}
+        onPress={() => this.props.navigation.navigate('CouponView')}>
+        <RkCard rkType='imgBlock' style={styles.card}>
+          <Image rkCardImg source={{ uri: `${UIConstants.ApiHost}${image.path}` }} style={{height: 180}} />
+          <View rkCardImgOverlay rkCardContent style={styles.overlay}>
+            <RkText rkType='header4 inverseColor'>{coupon.title}</RkText>
+            <RkText style={styles.time}
+              rkType='secondary2 inverseColor'>{coupon.value + ''} đ - {coupon.price + ''} đ</RkText>
+            <Image source={{ uri: '../../img/photo32.jpg' }} />
+          </View>
+          <View style={{ paddingVertical: 15, }}>
+            <View style={{ alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row', marginHorizontal: 50, }}>
+              <RkText rkType='awesome primary' onPress={() => this.like()}>
+                {this.state.isLiked ? FontAwesome.heart : FontAwesome.emptyHeart}
+              </RkText>
+              <RkText style={{ marginLeft: 30, }} rkType='awesome primary' onPress={() => this.props.navigation.navigate('Comments')}>
+                {FontAwesome.comment}
+              </RkText>
+            </View>
+          </View >
+        </RkCard>
+      </TouchableOpacity>
+    )
+  }
 
-        )
-    }
+  render() {
+    const { data } = this.state
+
+    return (
+      <FlatList
+        data={data}
+        renderItem={this.renderItem}
+        keyExtractor={this._keyExtractor}
+        style={styles.container} />
+
+    )
+  }
 }
 
 let styles = RkStyleSheet.create(theme => ({
-    container: {
-        backgroundColor: theme.colors.screen.scroll,
-        paddingVertical: 8,
-        paddingHorizontal: 14
-    },
-    card: {
-        marginVertical: 8,
-    },
-    time: {
-        marginTop: 5
-    }
+  container: {
+    backgroundColor: theme.colors.screen.scroll,
+    paddingVertical: 8,
+    paddingHorizontal: 14
+  },
+  card: {
+    marginVertical: 8,
+  },
+  time: {
+    marginTop: 5
+  }
 }));
